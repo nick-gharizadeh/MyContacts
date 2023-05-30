675d508d2322293154501341fab0985c1c8f37bd
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-05-30 07:57:36 +0330

    fea / add contact entity

--- app/src/main/java/com/example/mycontacts/data/model/Contact.kt
+++ app/src/main/java/com/example/mycontacts/data/model/Contact.kt
@@ -1,9 +1,14 @@
 package com.example.mycontacts.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+
+@Entity
 data class Contact(
-    val id: Int,
-    val fullName: String,
-    val phoneNumber: String,
+    @PrimaryKey val id: Int,
+    @ColumnInfo val fullName: String,
+    @ColumnInfo val phoneNumber: String,
 )
 
