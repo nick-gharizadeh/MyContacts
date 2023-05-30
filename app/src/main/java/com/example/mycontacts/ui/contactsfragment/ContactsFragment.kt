6bf04f0cf8aa86d75713870ee8c65e09f5d1d7b5
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-05-30 22:09:42 +0330

    imp / new codes for grant permission to delete deprecated codes

--- app/src/main/java/com/example/mycontacts/ui/contactsfragment/ContactsFragment.kt
+++ app/src/main/java/com/example/mycontacts/ui/contactsfragment/ContactsFragment.kt
@@ -1,14 +1,12 @@
 package com.example.mycontacts.ui.contactsfragment
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.mycontacts.databinding.FragmentContactsBinding
@@ -16,7 +14,6 @@ import com.example.mycontacts.databinding.FragmentContactsBinding
 
 class ContactsFragment : Fragment() {
     private lateinit var binding: FragmentContactsBinding
-    private val PERMISSIONS_REQUEST_READ_CONTACTS = 100
     private val contactsViewModel: ContactsViewModel by viewModels()
 
     override fun onCreateView(
@@ -29,41 +26,22 @@ class ContactsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        checkPermissionGranted()
+        requestPermission.launch(Manifest.permission.READ_CONTACTS)
+
     }
 
-    private fun checkPermissionGranted() {
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.READ_CONTACTS),
-                PERMISSIONS_REQUEST_READ_CONTACTS
-            )
-        } else {
-            val contacts = contactsViewModel.getContacts(requireContext().contentResolver)
-            Toast.makeText(requireContext(), "$contacts", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_READ_CONTACTS -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    val contacts = contactsViewModel.getContacts(requireContext().contentResolver)
-                    Toast.makeText(requireContext(), "$contacts", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-                return
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                val contacts = contactsViewModel.getContacts(requireContext().contentResolver)
+                Toast.makeText(requireContext(), "$contacts", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Permission denied! Please grant the permission ... :)",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
-    }
-
 
 }
