708e07e5de5523875b168354c4dd3f3222c3da64
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-05-31 08:52:58 +0330

    fea / add contacts adapter

---
+++ app/src/main/java/com/example/mycontacts/ui/contactsfragment/ContactsAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.mycontacts.ui.contactsfragment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mycontacts.R
+import com.example.mycontacts.data.model.Contact
+
+
+class ContactsAdapter :
+    ListAdapter<Contact, ContactsAdapter.ViewHolder>(ContactDiffCallback) {
+
+    object ContactDiffCallback : DiffUtil.ItemCallback<Contact>() {
+
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textViewContactName: TextView = view.findViewById(R.id.textView_contact_name)
+
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.contact_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val contact = getItem(position)
+        holder.textViewContactName.text = contact.fullName
+
+    }
+}
+
+
